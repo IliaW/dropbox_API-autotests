9be37e14d5f7a82fa8d3e3389fe16543ac58d416
IliaW <vovkilia@gmail.com>
2020-06-30 22:26:06 +0300

    Update AccessToken.java

--- src/main/java/com/dropbox/data/AccessToken.java
+++ src/main/java/com/dropbox/data/AccessToken.java
@@ -2,5 +2,5 @@ package com.dropbox.data;
 
 public class AccessToken {
 
-   public static final String BASIC_USER = "8ZNZgYBNSgAAAAAAAAACIWdmNhh13zDUMxorb-j6tgwlO_FCvBdxaeAwy9urWqQJ";
+   public static final String BASIC_USER = "8ZNZgYBNSgAAAAAAAAACQbYI4Q0CZYSt329uo5KdVyPejOM7MuFd1WlWPRCx_nv2";
 }
